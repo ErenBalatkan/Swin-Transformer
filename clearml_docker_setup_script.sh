dc0695f1bee0140da34449067b7be5e4c919222b
Eren <e.balatkan@gmail.com>
2021-07-21 14:51:54 +0300

    Updates

--- clearml_docker_setup_script.sh
+++ clearml_docker_setup_script.sh
@@ -2,6 +2,7 @@ apt-get update
 apt-get install ffmpeg libsm6 libxext6  -y
 pip install mmcv-full
 pip install terminaltables
+pip install termcolor
 pip install timm
 pip install albumentations
 pip install wandb
@@ -13,6 +14,10 @@ mkdir datasets
 wandb login 126df13111573362ac4698f1eacd2e1b31e50a45
 
 clearml-data get --id ac705e35976947d3ace2283fbb11a87b
-mkdir datasets/iccv
-mkdir datasets/iccv/val
-ln -s  datasets/iccv/val /clearml_agent_cache/storage_manager/datasets/ds_ac705e35976947d3ace2283fbb11a87b
+mkdir /datasets/iccv
+mkdir /datasets/iccv/val
+ln -s  /datasets/iccv/val /clearml_agent_cache/storage_manager/datasets/ds_ac705e35976947d3ace2283fbb11a87b
+
+mkdir /pretrained
+clearl-data get --id 842ad2a42bc340418f943b25462b7b13
+ln -s /pretrained /clearml_agent_cache/storage_manager/datasets/ds_842ad2a42bc340418f943b25462b7b13
--- clearml_launch_experiment.sh
+++ clearml_launch_experiment.sh
@@ -2,7 +2,7 @@ clearml-task \
 --project "Test" \
 --name "Test" \
 --docker nvcr.io/nvidia/pytorch:21.06-py3 \
---docker_bash_setup_script docker_setup_script.sh \
+--docker_bash_setup_script clearml_docker_setup_script.sh \
 --docker_args="--shm-size=16g" \
 --folder Swin-Transformer \
 --requirements requirements.txt \
