f82961cac98d840d7fb19c8438f7f1b2f1e90dcc
Eren <e.balatkan@gmail.com>
2021-07-21 19:49:47 +0300

    Updates

--- clearml_docker_setup_script.sh
+++ clearml_docker_setup_script.sh
@@ -10,14 +10,18 @@ pip install clearml
 pip uninstall -y pycocotools
 pip install mmpycocotools
 pip install yacs
-mkdir datasets
+pip install kornia
+
 wandb login 126df13111573362ac4698f1eacd2e1b31e50a45
 
+mkdir -p /datasets/iccv/val
+clearml-data get --id 04fe1e704c1a424bbe941d8f95f23fd8
+ln -s  /datasets/iccv/val /clearml_agent_cache/storage_manager/datasets/ds_04fe1e704c1a424bbe941d8f95f23fd8
+
+mkdir -p /datasets/iccv/test
 clearml-data get --id ac705e35976947d3ace2283fbb11a87b
-mkdir /datasets/iccv
-mkdir /datasets/iccv/val
 ln -s  /datasets/iccv/val /clearml_agent_cache/storage_manager/datasets/ds_ac705e35976947d3ace2283fbb11a87b
 
-mkdir /pretrained
-clearl-data get --id 842ad2a42bc340418f943b25462b7b13
-ln -s /pretrained /clearml_agent_cache/storage_manager/datasets/ds_842ad2a42bc340418f943b25462b7b13
+mkdir -p /datasets/iccv/train
+clearml-data get --id 2f8e31b466494869bddc88d100eded5c
+ln -s  /datasets/iccv/train /clearml_agent_cache/storage_manager/datasets/ds_2f8e31b466494869bddc88d100eded5
